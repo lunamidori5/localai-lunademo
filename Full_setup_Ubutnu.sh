476dabf4c7f04b1158ef31a1f1a8a6f1adec901f
LunaMidori5 <jaredteam@gmail.com>
2023-08-28 14:59:13 -0700

    Yea yea need a commite comment, bla bla

--- Full_setup_Ubutnu.sh
+++ Full_setup_Ubutnu.sh
@@ -2,7 +2,7 @@
 
 echo "You are on Ubuntu. Installing Docker, Python3.11"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -11,12 +11,13 @@ echo \
 "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.11
-sudo apt install python3.10-venv
+sudo apt install python3.11 -y
+sudo apt install python3.10-venv -y
 sudo apt-get install pip -y
+sudo apt install docker-compose
 
 echo "Setting up Python environment"
 python3 -m venv pythonlocalai
