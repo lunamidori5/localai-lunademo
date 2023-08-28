5e4238d5bbcb064990d23cc32e3896a612ff8016
LunaMidori5 <jaredteam@gmail.com>
2023-08-28 14:55:28 -0700

    Changed file names and added Fullsetup file

---
+++ Full_setup_Ubutnu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "You are on Ubuntu. Installing Docker, Python3.11"
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.11
+sudo apt install python3.10-venv
+sudo apt-get install pip -y
+
+echo "Setting up Python environment"
+python3 -m venv pythonlocalai
+source pythonlocalai/bin/activate
+
+echo "Installing Python Imports for LocalAi Demo Bot"
+pip install openai
+pip install langchain
+python main.py
---
+++ Min_Setup_Linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Setting up Python environment"
+python3 -m venv pythonlocalai
+source pythonlocalai/bin/activate
+
+echo "Installing Python Imports for LocalAi Demo Bot"
+pip install openai
+pip install langchain
+python main.py
