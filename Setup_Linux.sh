5d3b2d43ecbeca7e678730c6541c8b3727c19047
LunaMidori5 <jaredteam@gmail.com>
2023-10-04 17:52:20 -0700

    added a way to remove the txt files

--- Setup_Linux.sh
+++ Setup_Linux.sh
@@ -4,6 +4,8 @@ python3 -m venv pythonlocalai
 source pythonlocalai/bin/activate
 
 echo "Installing Python Imports for LocalAi Demo Bot"
+rm chatlog.log
+rm already_setup.txt
 pip install openai 
 pip install langchain
 pip install requests
